81b1713162c24d1dd966c3612786e9743909c42d
hanzhangang-bin <156191458@qq.com>
2026-03-05 17:30:16 +0800

    fix: parse toc field-code headings for import

---
+++ backend/src/main/java/com/rdms/mapper/DocCatalogMapper.java
@@ -0,0 +1,7 @@
+package com.rdms.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.rdms.entity.DocCatalog;
+
+public interface DocCatalogMapper extends BaseMapper<DocCatalog> {
+}
